cd430456f01d38588d25fb1cd76b680651c3dcfb
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2020-01-28 10:21:26 +0700

    simple access client

--- src/http/Client.php
+++ src/http/Client.php
@@ -2,14 +2,35 @@
 namespace ValidatorPizza\Http;
 
 use RuntimeException;
+use ValidatorPizza\Http\Utils\Client as HttpClient;
 
 /**
  * http
  */
 class Client
 {
-	public function get()
+	protected $http;
+
+	public function __construct()
 	{
-		# code...
+		$this->http = new HttpClient();
+	}
+
+	public static function get($url, $params = [])
+	{
+		$self = new self();
+		return $self->get($url, $params);
+	}
+
+	public static function post($url, $data)
+	{
+		$self = new self();
+		return $self->post($url, $data);
+	}
+
+	public static function request($method, $url, array $data = [])
+	{
+		$self = new self();
+		return $self->request($method, $url, $data);
 	}
 }
